cd79585fe65fc01521a59b20b3550612ba800c41
sajad <sajadph79@gmail.com>
2021-09-09 14:34:16 +0430

    add models Transaction and Block without migrate

--- chain/models.py
+++ chain/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 from django.db.models.base import Model
 
-    
+class Transaction(models.Model):
+    sender = models.CharField(max_length=64)
+    receiver = models.CharField(max_length=64)
+    timestamp = models.DateTimeField()
+    value = models.FloatField()
+
+class Block(models.Model):
+    hash_block = models.CharField(max_length=256)
+    prev_block = models.ForeignKey("Block", on_delete=models.PROTECT, unique=True)
+    transactions = models.ManyToManyField("Transaction", related_name="block_transactions", unique=True)
+    timestamp = models.DateTimeField()
+    difficulty = models.FloatField()
+    nonce = models.IntegerField()    
