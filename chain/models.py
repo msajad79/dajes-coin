434dd8a4407e90fd452fe33cea359f76c3ba58a8
sajad <sajadph79@gmail.com>
2021-09-09 14:53:35 +0430

    edited Block model

--- chain/models.py
+++ chain/models.py
@@ -9,8 +9,8 @@ class Transaction(models.Model):
 
 class Block(models.Model):
     hash_block = models.CharField(max_length=256)
-    prev_block = models.ForeignKey("Block", on_delete=models.PROTECT, unique=True)
-    transactions = models.ManyToManyField("Transaction", related_name="block_transactions", unique=True)
+    prev_block = models.OneToOneField("Block", on_delete=models.PROTECT)
+    transactions = models.ManyToManyField("Transaction", related_name="block_transactions")
     timestamp = models.DateTimeField()
     difficulty = models.FloatField()
     nonce = models.IntegerField()    
